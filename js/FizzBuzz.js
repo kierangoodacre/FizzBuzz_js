daf0b50b6e97cdc8247839709ec7ee225dafe4b2
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-20 12:28:20 +0000

    created FizzBuzz game

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -3,3 +3,26 @@ var FizzBuzz = function() {};
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
 	return number % 3 === 0;
 };
+
+FizzBuzz.prototype.isDivisibleByFive = function(number){
+	return number % 5 === 0;
+};
+
+FizzBuzz.prototype.isDivisibleByFifteen = function(number){
+	return number % 15 === 0;
+};
+
+FizzBuzz.prototype.play = function(number){
+	if (this.isDivisibleByFifteen(number)) {
+		return "FizzBuzz";
+	}
+	else if (this.isDivisibleByThree(number)) {
+		return "Fizz"; 
+	}
+	else if (this.isDivisibleByFive(number)) {
+		return "Buzz";
+	}
+	else {
+	return number;
+	}
+};
--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -15,6 +15,45 @@ describe("FizzBuzz", function() {
 			expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
 		});
 
+		it("it is divisible by 5", function() {
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+		});
+
+		it("it is NOT divisible by 5", function() {
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.isDivisibleByFive(7)).toBe(false);
+		});
+
+		it("", function(){
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+		});
+
 	}); 
+
+	describe("Returns, Fizz, Buzz or FizzBuzz", function(){
+
+		it("return Fizz when divisible by 3", function(){
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.play(3)).toEqual("Fizz");
+		});
+
+		it("to Not return Fizz when not divisible by 3", function(){
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.play(1)).toEqual(1);
+		});
+
+		it("To return Buzz when divisible by 5", function(){
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.play(5)).toEqual("Buzz");
+		});
+
+		it("To return FizzBuzz when divisible by 15", function(){
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.play(30)).toEqual("FizzBuzz");
+		});
+
+	});
 
 });
